4e1f66d466cdc7f5b75e95507c5d3cc0f52d3e0a
Olayanju Oluwadamilare <hadreytechsolutions@gmail.com>
2023-10-20 17:06:12 +0100

    Completed Assignemnt

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,18 @@
 class Song:
+    
+    count = 0
+    
+    def __init__(self,name,artist,genres):
+        self.name = name 
+        self.artist = artist
+        self.genres = genres
+        
+    def add_song_to_count(self):
+        self.count += 1
+    
+    def add_to_genres(self):
+        self.genres = []
+        
+    def add_to_artists(self):
+        self.artist = []
     pass
